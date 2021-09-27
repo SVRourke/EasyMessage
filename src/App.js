6e6086835dbf10e8abc93174ae98cc129e5bc101
SVRourke <samuelvanrourke@gmail.com>
2021-09-26 20:00:00 -0600

    stub out routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    
+      <div>
+        <ul>
+          <li>
+            <Link to="/template">Template</Link>
+          </li>
+          <li>
+            <Link to="/form">Form</Link>
+          </li>
+        </ul>
+
+
+        <Switch>
+          <Route path="/template">
+            <h1>Hello Template</h1>
+          </Route>
+          <Route path="/form">
+            <h1>Hello Form</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
