8d22e88c3fe6a4153dc4ea09ec404928238a8176
SVRourke <samuelvanrourke@gmail.com>
2021-09-28 08:19:12 -0400

    switched to replaceAll

--- src/templatePrep.js
+++ src/templatePrep.js
@@ -1,8 +1,8 @@
 const templatePrepper = (event, template) => {
   const [name, company] = event.target;
   const content = template
-    .replace("#NAME", name.value)
-    .replace("#COMPANY", company.value);
+    .replaceAll("#NAME", name.value)
+    .replaceAll("#COMPANY", company.value);
   return content;
 };
 
